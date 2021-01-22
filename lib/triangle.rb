2478af4db75d458b29b1eb97c60a9412cbc0da5d
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-22 10:28:44 -0800

    commit tree 98bed81fb743c38b8b99870c2c7afd4f02df3577

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,54 @@
 class Triangle
-  # write code here
+
+  attr_accessor :s1, :s2, :s3 
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 || s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil || s3 == nil
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 && @s2 != @s3 
+      return :isosceles
+    elsif @s1 == @s3 && @s2 != @s1
+      return :isosceles
+    elsif @s3 == @s2 && @s2 != @s1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+
+
+  class TriangleError < StandardError
+    def message
+      "You must give the triangle method a valid triangle or face the consequences!"
+    end
+  end
 end
+
+
+  #   if @length1 + @length2 < @length3 || @length1 + @length3 < @length2 || @length2 + @length3 < @length1 || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
+  #     begin
+  #       raise TriangleError
+  #     rescue TriangleError => error
+  #       puts error.message
+  #     end
+  #   end
+  # end
